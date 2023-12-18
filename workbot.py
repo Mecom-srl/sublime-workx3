bf2378829c033a24714b24cc456d9d36b3a70ffc
meska <meskatech@gmail.com>
2023-12-18 19:25:26 +0100

    asyncronous version

--- workbot.py
+++ workbot.py
@@ -1,5 +1,9 @@
-import sublime
-import sublime_plugin
+try:
+    import sublime
+    import sublime_plugin
+except ImportError:
+    pass
+
 import threading
 import os
 import socket
@@ -7,11 +11,11 @@ import requests
 import getpass
 from hashlib import sha224
 from time import sleep
-__version__ = '1.8'
+
+__version__ = '1.9'
 settings_file = 'WorkX3.sublime-settings'
 settings = {}
 PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
-
 
 hostkey = sha224(("%s %s" % (getpass.getuser(), socket.gethostname())).encode()).hexdigest()
 
@@ -36,27 +40,30 @@ def log(lvl, message, *args, **kwargs):
         sublime.set_timeout(lambda: log(lvl, message, *args, **kwargs), 0)
 
 
-class Listener(threading.Thread):
-    def __init__(self, r, channels):
+# classe thread che fa le richieste via http
+class WorkBotThread(threading.Thread):
+    def __init__(self, text):
+        self.text = text
         threading.Thread.__init__(self)
-        self.redis = r
-        self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe(channels)
-
-    def work(self, item):
-        if isinstance(item['data'], bytes):
-            print(item['data'].decode('utf-8'))
-        # print(item['channel'], ":", item['data'])
 
     def run(self):
-        for item in self.pubsub.listen():
-            if item['data'] == b"KILL":
-                self.pubsub.unsubscribe()
-                log(WARNING, 'Killed!')
-                print(self, "unsubscribed and finished")
-                break
+        params = dict(
+            user=hostkey
+        )
+        for riga in self.text.splitlines():
+            if riga.strip() == '':
+                continue
+            params['text'] = riga.strip()
+            r = requests.post(settings.get("host"), params, timeout=60)
+
+            if r.status_code == requests.codes.ok:
+                for line in r.text.splitlines():
+                    print(line)
             else:
-                self.work(item)
+                log(ERROR, 'Errore invio a Work, riprovare')
+                log(ERROR, r.text)
+                sleep(1)
+            sleep(0.5)
 
 
 class WorkSendToBotCommand(sublime_plugin.TextCommand):
@@ -68,29 +75,9 @@ class WorkSendToBotCommand(sublime_plugin.TextCommand):
             # if the user didn't select anything, search the currently highlighted word
             if selection.empty():
                 text = self.view.word(selection)
-
             text = self.view.substr(selection)
-
-            params = dict(
-                user=hostkey
-            )
-            for riga in text.splitlines():
-                if riga.strip() == '':
-                    continue
-                params['text'] = riga.strip()
-                r = requests.post(settings.get("host"), params, timeout=60)
-
-                if r.status_code == requests.codes.ok:
-                    for line in r.text.splitlines():
-                        print(line)
-                else:
-                    log(ERROR, 'Errore invio a Work, riprovare')
-                    log(ERROR, r.text)
-                    sleep(1)
-                sleep(0.5)
-
-
-
+            log(INFO, 'Invio comandi a Work ...')
+            WorkBotThread(text).start()
 
 
 def plugin_loaded():
